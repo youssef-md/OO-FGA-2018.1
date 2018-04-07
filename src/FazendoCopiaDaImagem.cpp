0c31dfc7e622138dd67ebf870a3347526b8faf21
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-04-07 15:00:23 -0300

    Usando a classe vector, ao inves de uma matriz,  para armazenar a imagem lida

--- src/FazendoCopiaDaImagem.cpp
+++ src/FazendoCopiaDaImagem.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
-
-void criarImagem();
-
 int main() {
 
-	//criarImagem();
 
 	string filename;
 	getline(cin, filename);
  
 	ifstream imgIn;
 	imgIn.open(filename);
-	
+
 
 	ofstream imgOut;
 	string filename_copy =  "copy_of_" + filename;
 	imgOut.open(filename_copy);
 	
+	vector<int> imgVector;
+
 	if(imgIn.is_open()) {
 
 		char character;
+
 		while(imgIn.get(character)) {
 			
-			imgOut << character;	
+			imgOut << character;
+			imgVector.push_back(character);
 		}
 
 	}else {
@@ -34,35 +35,11 @@ int main() {
 		cout << "erro" << endl;
 	}
 
+	cout << "tamanho do vector: " << imgVector.size() << endl;
+
 	imgIn.close();
 	imgOut.close();
 
 
 	return 0;
-}
-
-void criarImagem() {
-
-	int width = 200, height = 200;
-
-	fstream image;
-	image.open("picture.ppm", ios::out);
-
-	image << "P3" << endl;
-	image << width << " " << height << endl;
-	image << "255" << endl;
-
-	
-	for(int row = 0; row < height; row++) {
-		for(int column = 0; column < width; column++) {
-
-			int r = row % 200;
-			int g = column % 200;
-			int b = column * row % 200;
-
-			image << r << " " << g << " " << b << endl;
-		}
-	}
-
-	image.close();
 }

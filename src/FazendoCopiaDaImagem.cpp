7ff591d69d959b448b3e1ea06587a760a6c15ebc
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-04-10 00:07:15 -0300

    ignorando o # e salvando a linha após o # em um arquivo txt

--- src/FazendoCopiaDaImagem.cpp
+++ src/FazendoCopiaDaImagem.cpp
@@ -18,15 +18,40 @@ int main() {
 	string filename_copy =  "copy_of_" + filename;
 	imgOut.open(filename_copy);
 	
+	//ofstream de teste da leitura da config
+	ofstream configLine;
+	configLine.open("configLine.txt");
+
 
 	vector<char> imgVector;
 
+	vector<int> imgConfig;
+
+	char charOfConfig;
 	if(imgIn.is_open()) { //copiando a imagem para o vector
 
 		char character;
+		int counter = 0;
 		while(imgIn.get(character)) {
 
-			imgVector.push_back(character);
+			if(counter >= 3 && counter <= 10) {
+				if(character == '#'){
+
+					while(imgIn.get(charOfConfig)){
+						if(charOfConfig != '\n')
+							configLine << charOfConfig;
+						else
+							break;		
+					}
+			
+				}else {
+					imgVector.push_back(character);	
+				}
+			}
+			else {
+				imgVector.push_back(character);	
+			}
+			counter ++;
 		}
 
 	}else {
@@ -37,14 +62,21 @@ int main() {
 	long int sizeVector = imgVector.size();
 	cout << "tamanho do vector: " << sizeVector << endl;
 
-	for(int i = 0; i < sizeVector; i++) {
+	for(int i = 0; i < sizeVector; i++) { //copiando o vector na saida 
 
 		imgOut << imgVector[i];
 	}
 
+/*
+	int sizeConfigLine = imgConfig.size();
+	for(int i = 0; i < sizeConfigLine; i++) { //copiando o vector na saida 
+
+		configLine << imgConfig[i];
+	}
+*/
 	imgIn.close();
 	imgOut.close();
-
+	configLine.close();
 
 	return 0;
 }

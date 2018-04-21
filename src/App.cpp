d25b2bd37410afd4dae6d547b59cc9d533e6b0f8
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-04-21 00:54:55 -0300

    Exceção ao rodar a imagem .pgm, invalid_argument what() stoi

---
+++ src/App.cpp
@@ -0,0 +1,41 @@
+#include "Image.hpp"
+#include "PGM.hpp"
+#include "PPM.hpp"
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int main() {
+
+	string filename;
+	
+	cout << "nome do arquivo: ";
+	
+	getline(cin, filename);
+
+	if(filename.find(".pgm") != string::npos) {
+
+		PGM * imgPGM = new PGM(filename);
+		imgPGM->decrypt();
+
+		delete imgPGM;
+
+	} else if(filename.find(".ppm") != string::npos) {
+
+		PPM * imgPPM = new PPM(filename);
+		imgPPM->decrypt();
+		
+		delete imgPPM;
+
+	} else {
+
+		throw invalid_argument(".pgm or .ppm was not identified");
+	}
+
+	
+	
+
+	return 0;
+}

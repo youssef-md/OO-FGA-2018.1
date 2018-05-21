715366e3f19acb3fc1458bd37cef5b64d3653034
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-05-21 00:04:30 -0300

    Criação do canvas e adicionando no JFrame

--- EP2/src/dev/ep2/battleship/Launcher.java
+++ EP2/src/dev/ep2/battleship/Launcher.java
@@ -1,10 +1,9 @@
 package dev.ep2.battleship;
 
-import dev.ep2.battleship.display.Display;
-
 public class Launcher {
 
 	public static void main(String[] args) {
-		new Display("Battleship", 800, 800);
+		
+		new Game("Battleship", 800, 800);
 	}
 }

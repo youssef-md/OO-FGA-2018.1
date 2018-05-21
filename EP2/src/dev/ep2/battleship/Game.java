2650f2568b49cbd8438d7cd1fa5883edb876cf79
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-05-21 00:29:45 -0300

    Implementação da sobrescrita run() da classe Runnable com o game loop, e tratando os possíveis erros nos métodos sync start() e sync stop()

--- EP2/src/dev/ep2/battleship/Game.java
+++ EP2/src/dev/ep2/battleship/Game.java
@@ -5,42 +5,71 @@ import dev.ep2.battleship.display.Display;
 public class Game implements Runnable {
 
 	public int width, height;
-	
+	public String title;
+	private boolean running = false;;
 	private Display display;
 	private Thread thread;
 	
 	
 	
-	
-	
 	public Game(String title, int width, int height) {
 		
+		this.title = title;
 		this.width = width;
 		this.height = height;
+	}
+
+	
+	private void init() {
 		
 		display = new Display(title, width, height);
 	}
-
+	
+	private void update() {
+		
+	}
+	
+	private void render() {
+		
+	}
 
 	@Override
 	public void run() {
 	
+		init(); 
 		
+		while(running) { // game loop
+			
+			update();
+			render();
+		}
+		
+		stop();
 	}
 	
 	public synchronized void start() {
 		
-		thread = new Thread(this); // running Game class(this) as a Thread
-		thread.start(); // calling run()
+		if(!running) {
+			
+			running = true;
+			thread = new Thread(this); // running Game class(this) as a Thread
+			thread.start(); // calling run()
+		}			
 	}
 	
 	public synchronized void stop() {
 		
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		if(running) {
+			
+			running = false;
+			
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}	
 		}
+		
 	}
 	
 }

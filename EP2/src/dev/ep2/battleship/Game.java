46233b20b7cd9f990660938e8de6ddfd9e42cde4
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-05-21 00:17:30 -0300

    Criação do objeto thread e a implementação dos métodos sync start e sync stop

--- EP2/src/dev/ep2/battleship/Game.java
+++ EP2/src/dev/ep2/battleship/Game.java
@@ -2,11 +2,16 @@ package dev.ep2.battleship;
 
 import dev.ep2.battleship.display.Display;
 
-public class Game {
+public class Game implements Runnable {
 
-	private Display display;
-	
 	public int width, height;
+	
+	private Display display;
+	private Thread thread;
+	
+	
+	
+	
 	
 	public Game(String title, int width, int height) {
 		
@@ -15,4 +20,27 @@ public class Game {
 		
 		display = new Display(title, width, height);
 	}
+
+
+	@Override
+	public void run() {
+	
+		
+	}
+	
+	public synchronized void start() {
+		
+		thread = new Thread(this); // running Game class(this) as a Thread
+		thread.start(); // calling run()
+	}
+	
+	public synchronized void stop() {
+		
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }

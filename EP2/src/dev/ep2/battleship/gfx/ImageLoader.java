c8b522228069d0463205ca8166eb6535848ea067
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-05-21 23:34:12 -0300

    ImageLoader class: toda a parte de carregamento de imagens

---
+++ EP2/src/dev/ep2/battleship/gfx/ImageLoader.java
@@ -0,0 +1,21 @@
+package dev.ep2.battleship.gfx;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageLoader {
+
+	public static BufferedImage loadImage(String path) {
+		
+		try {
+			return ImageIO.read(ImageLoader.class.getResource(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1); //The path/img doesn't exist
+		}
+		
+		return null;
+	}
+}

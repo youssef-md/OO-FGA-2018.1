00c65ae5a29c05cec1a56d3a59b8c0d8ce5d5e45
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-05-23 11:30:12 -0300

    Utilizando a Player class na GameState

--- EP2/src/dev/ep2/battleship/states/GameState.java
+++ EP2/src/dev/ep2/battleship/states/GameState.java
@@ -2,25 +2,28 @@ package dev.ep2.battleship.states;
 
 import java.awt.Graphics;
 
-import dev.ep2.battleship.gfx.Assets;
+import dev.ep2.battleship.entities.creatures.Player;
 
 public class GameState extends State{
 
+	private Player player;
+	
 	public GameState() {
 		
-		
+		player = new Player(250, 250);
 	}
 	
 	
 	@Override
 	public void tick() {
 	
+		player.tick();
 	}
 
 	@Override
 	public void render(Graphics g) {
 		
-	
+		player.render(g);
 	}
 
 	

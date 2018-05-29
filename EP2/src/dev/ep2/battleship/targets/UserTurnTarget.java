e6adbaff6e57992312b538af1b8e3bd024ef2764
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-05-29 00:56:07 -0300

    Alvo de cor padrão, usado para inicializar o jogo e para simbolizar o turno do usuário

---
+++ EP2/src/dev/ep2/battleship/targets/UserTurnTarget.java
@@ -0,0 +1,11 @@
+package dev.ep2.battleship.targets;
+
+import dev.ep2.battleship.gfx.Assets;
+
+public class UserTurnTarget extends Target {
+
+	public UserTurnTarget(int ID) {
+		
+		super(Assets.user_turn_target, ID);
+	}
+}

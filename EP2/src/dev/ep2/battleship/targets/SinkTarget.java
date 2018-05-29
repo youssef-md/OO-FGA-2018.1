472122fa82f7c846ab5d393539b7985335e5b3f2
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-05-29 00:58:43 -0300

    Alvo de cor vermelha, usado para identificar quando o usuário afundou toda uma embarcação

---
+++ EP2/src/dev/ep2/battleship/targets/SinkTarget.java
@@ -0,0 +1,12 @@
+package dev.ep2.battleship.targets;
+
+import dev.ep2.battleship.gfx.Assets;
+
+public class SinkTarget extends Target {
+	
+	public SinkTarget(int ID) {
+		
+		super(Assets.sink_target, ID);
+	}
+
+}

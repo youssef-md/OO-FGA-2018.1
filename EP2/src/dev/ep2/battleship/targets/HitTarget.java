15077202d921530359c5b9104103a0dc5c8ffaa7
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-05-29 00:57:56 -0300

    Alvo de cor laranja, usado para identificar quando o usuário acertar uma parte da embarcação

---
+++ EP2/src/dev/ep2/battleship/targets/HitTarget.java
@@ -0,0 +1,12 @@
+package dev.ep2.battleship.targets;
+
+import dev.ep2.battleship.gfx.Assets;
+
+public class HitTarget extends Target {
+
+	public HitTarget(int ID) {
+		
+		super(Assets.hit_target, ID);
+	}
+
+}

7e734f46240c1d9a1e5ddb11f4b4b09d9f6f7c01
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-05-30 09:45:01 -0300

    FileHelper class para realizar a leitura do mapa

---
+++ EP2/src/dev/ep2/battleship/helpers/FileHelper.java
@@ -0,0 +1,58 @@
+package dev.ep2.battleship.helpers;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileHelper {
+
+	private static BufferedReader bufferReader;
+	private FileReader fileReader;
+	
+	public FileHelper(String path) {
+		
+		try {
+			
+			fileReader = new FileReader(path);
+			bufferReader = new BufferedReader(fileReader);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getnumberOfTargetXandY() {
+				
+		int numberOfTargetX = 0;
+		int numberOfTargetY = 0;
+		String line = "";
+		StringBuilder builder = new StringBuilder();
+		
+		try {
+			
+			bufferReader.readLine();
+			line = bufferReader.readLine();
+			builder.append(line + "\n");
+			
+			System.out.println(builder);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return builder.toString();
+		
+	}	
+	
+	
+	public static int parseInt(String number) {
+		
+		try {
+			return Integer.parseInt(number);
+		} catch(NumberFormatException e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+}

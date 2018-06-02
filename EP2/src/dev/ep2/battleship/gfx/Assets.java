d8b2f07e8bbffa973c01be9f6604db23c1791c28
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-02 01:37:57 -0300

    Identificando as ações de hover e de click nos botões Load e Start, e mudando de estato ao clicar no botão start

--- EP2/src/dev/ep2/battleship/gfx/Assets.java
+++ EP2/src/dev/ep2/battleship/gfx/Assets.java
@@ -7,19 +7,25 @@ import javax.swing.ImageIcon;
 
 public class Assets {
 
-	public static BufferedImage user_turn_target, pc_turn_target, hit_target, sink_target, filter;
+	public static BufferedImage user_turn_target, pc_turn_target, hit_target, sink_target, filter, btn_start, btn_load, game_title;
 	public Image wave_animation = new ImageIcon(getClass().getResource("/textures/wave_animation.gif")).getImage();
-	//public Image wave_animation = new ImageIcon(getClass().getResource("/textures/Sequence_02.gif")).getImage();
+	
 
-	private int WAVE_SIZE = 1000;
 	
 	public static void init() {
 
 		SpriteSheet filterSheet = new SpriteSheet(ImageLoader.loadImage("/textures/filter.png"));
-		
-		filter = filterSheet.crop(0, 0, 550, 200);
+		SpriteSheet gameTitleSheet = new SpriteSheet(ImageLoader.loadImage("/textures/game_title.png"));
+		SpriteSheet btnStartSheet = new SpriteSheet(ImageLoader.loadImage("/textures/btn_start.png"));
+		SpriteSheet btnLoadSheet = new SpriteSheet(ImageLoader.loadImage("/textures/btn_load.png"));
 		
 		SpriteSheet targets = new SpriteSheet(ImageLoader.loadImage("/textures/targets_sprite_sheet.png"));
+		
+		game_title = gameTitleSheet.crop(0, 0, 1200, 500);
+		btn_start = btnStartSheet.crop(0, 0, 454, 166);
+		btn_load = btnLoadSheet.crop(0, 0, 454, 166);
+		filter = filterSheet.crop(0, 0, 550, 200);
+		
 		
 		user_turn_target = targets.crop(0, 0, 200, 200);
 		pc_turn_target = targets.crop(200, 0, 200, 200);
--- EP2/src/dev/ep2/battleship/states/MenuView.java
+++ EP2/src/dev/ep2/battleship/states/MenuView.java
@@ -19,10 +19,34 @@ public class MenuView extends State	{
 	@Override
 	public void tick() {
 		
-		System.out.println("MouseX = " + handler.getMouseManager().getMouseX() + " MouseY = " + handler.getMouseManager().getMouseY());
-		if(handler.getMouseManager().isLeftPressed()) {
-			handler.getRoute().setView(handler.getGameView());		
+		//System.out.println("MouseX = " + handler.getMouseManager().getMouseX() + " MouseY = " + handler.getMouseManager().getMouseY());
+		
+		// load button
+		if(handler.getMouseManager().getMouseX() >= 530 && handler.getMouseManager().getMouseX() <= 862) {
+			if(handler.getMouseManager().getMouseY() >= 506 && handler.getMouseManager().getMouseY() <= 612) {
+				//hover
+				//color change and sound
+				
+				if(handler.getMouseManager().isLeftPressed()) { 
+					//click
+					
+				}
+			}
 		}
+		
+		// start button
+		if(handler.getMouseManager().getMouseX() >= 530 && handler.getMouseManager().getMouseX() <= 862) {
+			if(handler.getMouseManager().getMouseY() >= 669 && handler.getMouseManager().getMouseY() <= 766) {
+				//hover
+				//color change and sound
+				
+				if(handler.getMouseManager().isLeftPressed()) { 
+					//click
+					handler.getRoute().setView(handler.getGameView());
+				}
+			}
+		}
+		
 	
 	}
 
@@ -31,7 +55,11 @@ public class MenuView extends State	{
 		
 		
 		g.drawImage(assets.getIconWaveAnimation(), 0, 0, handler.getAppWidth(), handler.getAppHeight(), null);
+		g.drawImage(Assets.game_title, 210, 20, 1000, 400, null);
 		g.drawImage(Assets.filter, 0, 0, handler.getAppWidth(), handler.getAppHeight(), null);
+		
+		g.drawImage(Assets.btn_load, 520, 500, 350, 130, null);
+		g.drawImage(Assets.btn_start, 520, 650, 350, 130, null);
 		
 	}
 

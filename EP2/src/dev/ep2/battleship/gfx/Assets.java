1670b051fae3bf21eb9b0e058fe0a15c818b62ab
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-03 20:31:23 -0300

    Inicializando os spritesheets dos botões de estratégias, botoes de sexo do usuário e do respectivo avatar

--- EP2/src/dev/ep2/battleship/gfx/Assets.java
+++ EP2/src/dev/ep2/battleship/gfx/Assets.java
@@ -9,35 +9,48 @@ import javax.swing.ImageIcon;
 public class Assets {
 
 	public static BufferedImage user_turn_target, pc_turn_target, hit_target, sink_target, filter, btn_start, btn_start_hover, game_title;
-	public static BufferedImage popup_alert;
+	public static BufferedImage avatar_base, avatar_male, avatar_female, btn_sex, btn_male_hover, btn_male_pressed;
+	public static BufferedImage btn_fem_hover, btn_fem_pressed; 
 	public Image wave_animation = new ImageIcon(getClass().getResource("/textures/wave_animation.gif")).getImage();
 	public static Font military_font28;
 
 	
 	public static void init() {
 
-		SpriteSheet filterSheet = new SpriteSheet(ImageLoader.loadImage("/textures/filter.png"));
 		SpriteSheet gameTitleSheet = new SpriteSheet(ImageLoader.loadImage("/textures/game_title.png"));
+		SpriteSheet filterSheet = new SpriteSheet(ImageLoader.loadImage("/textures/filter.png"));
+		SpriteSheet targetsSheet = new SpriteSheet(ImageLoader.loadImage("/textures/targets_sprite_sheet.png"));
+		SpriteSheet userAvatarSheet  = new SpriteSheet(ImageLoader.loadImage("/textures/spritesheet_user_avatar.png"));
+		SpriteSheet btnSexSheet  = new SpriteSheet(ImageLoader.loadImage("/textures/spritesheet_btn_sex.png"));
+		
 		SpriteSheet btnStartSheet = new SpriteSheet(ImageLoader.loadImage("/textures/btn_start.png"));
 		SpriteSheet btnStartSheetHover = new SpriteSheet(ImageLoader.loadImage("/textures/btn_start::hover.png"));
-		SpriteSheet targets = new SpriteSheet(ImageLoader.loadImage("/textures/targets_sprite_sheet.png"));
-		SpriteSheet popups = new SpriteSheet(ImageLoader.loadImage("/textures/popup_alert.png"));
+		//SpriteSheet popups = new SpriteSheet(ImageLoader.loadImage("/textures/popup_alert.png"));
 		
-		military_font28 = FontLoader.loadFont("res/fonts/Don't Delay, Act Now!.otf", 28);
-		
-		game_title = gameTitleSheet.crop(0, 0, 1200, 500);
+				
 		
 		btn_start = btnStartSheet.crop(0, 0, 454, 166);
 		btn_start_hover = btnStartSheetHover.crop(0, 0, 454, 166);
 		
+		military_font28 = FontLoader.loadFont("res/fonts/Don't Delay, Act Now!.otf", 28);
+		game_title = gameTitleSheet.crop(0, 0, 1200, 500);
 		filter = filterSheet.crop(0, 0, 550, 200);
 		
-		popup_alert = popups.crop(0, 0, 1129, 585);
+		avatar_base = userAvatarSheet.crop(0, 0, 512, 512);
+		avatar_male = userAvatarSheet.crop(512, 0, 512, 512);
+		avatar_female = userAvatarSheet.crop(1024, 0, 512, 512);
+
+		btn_sex = btnSexSheet.crop(0, 0, 454, 166);
+		btn_male_hover = btnSexSheet.crop(454, 0, 454, 166);
+		btn_fem_hover = btnSexSheet.crop(908, 0, 454, 166);
+		btn_male_pressed = btnSexSheet.crop(1362, 0, 454, 166);
+		btn_fem_pressed = btnSexSheet.crop(1816, 0, 454, 166);
+
 		
-		user_turn_target = targets.crop(0, 0, 200, 200);
-		pc_turn_target = targets.crop(200, 0, 200, 200);
-		hit_target = targets.crop(400, 0, 200, 200);
-		sink_target = targets.crop(600, 0, 200, 200);
+		user_turn_target = targetsSheet.crop(0, 0, 200, 200);
+		pc_turn_target = targetsSheet.crop(200, 0, 200, 200);
+		hit_target = targetsSheet.crop(400, 0, 200, 200);
+		sink_target = targetsSheet.crop(600, 0, 200, 200);
 		
 		
 	}

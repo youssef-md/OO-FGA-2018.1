342f597c132a28f200094d595e13ec1cf3a9a8d8
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-02 21:57:07 -0300

    FontLoader e Text class: responsáveis por carregar uma fonte e desenhar no canvas respectivamente

--- EP2/src/dev/ep2/battleship/gfx/Assets.java
+++ EP2/src/dev/ep2/battleship/gfx/Assets.java
@@ -1,5 +1,6 @@
 package dev.ep2.battleship.gfx;
 
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 
@@ -7,9 +8,9 @@ import javax.swing.ImageIcon;
 
 public class Assets {
 
-	public static BufferedImage user_turn_target, pc_turn_target, hit_target, sink_target, filter, btn_start, btn_load, game_title, btn_load_hover, btn_start_hover;
+	public static BufferedImage user_turn_target, pc_turn_target, hit_target, sink_target, filter, btn_start, btn_start_hover, game_title;
 	public Image wave_animation = new ImageIcon(getClass().getResource("/textures/wave_animation.gif")).getImage();
-	
+	public static Font military_font28;
 
 	
 	public static void init() {
@@ -17,20 +18,15 @@ public class Assets {
 		SpriteSheet filterSheet = new SpriteSheet(ImageLoader.loadImage("/textures/filter.png"));
 		SpriteSheet gameTitleSheet = new SpriteSheet(ImageLoader.loadImage("/textures/game_title.png"));
 		SpriteSheet btnStartSheet = new SpriteSheet(ImageLoader.loadImage("/textures/btn_start.png"));
-		SpriteSheet btnLoadSheet = new SpriteSheet(ImageLoader.loadImage("/textures/btn_load.png"));
-		
 		SpriteSheet btnStartSheetHover = new SpriteSheet(ImageLoader.loadImage("/textures/btn_start::hover.png"));
-		SpriteSheet btnLoadSheetHover = new SpriteSheet(ImageLoader.loadImage("/textures/btn_load::hover.png"));
-		
 		SpriteSheet targets = new SpriteSheet(ImageLoader.loadImage("/textures/targets_sprite_sheet.png"));
+		
+		military_font28 = FontLoader.loadFont("res/fonts/Don't Delay, Act Now!.otf", 28);
 		
 		game_title = gameTitleSheet.crop(0, 0, 1200, 500);
 		
 		btn_start = btnStartSheet.crop(0, 0, 454, 166);
-		btn_load = btnLoadSheet.crop(0, 0, 454, 166);
-		
 		btn_start_hover = btnStartSheetHover.crop(0, 0, 454, 166);
-		btn_load_hover = btnLoadSheetHover.crop(0, 0, 454, 166);
 		
 		
 		filter = filterSheet.crop(0, 0, 550, 200);

011c49fcb6f28c8da0806a66ad310a8c37c1ea95
youssef-md <emaildeyoussefmuhamad@gmail.com>
2018-06-04 01:41:25 -0300

    Carregando o painel esquerdo, responsável por exibir as habilidades e a pontuação

--- EP2/src/dev/ep2/battleship/gfx/Assets.java
+++ EP2/src/dev/ep2/battleship/gfx/Assets.java
@@ -11,7 +11,7 @@ public class Assets {
 	public static BufferedImage user_turn_target, pc_turn_target, hit_target, sink_target, filter, btn_start, btn_start_hover, game_title;
 	public static BufferedImage avatar_base, avatar_male, avatar_female, btn_sex, btn_male_hover, btn_male_pressed;
 	public static BufferedImage btn_fem_hover, btn_fem_pressed, btn_rank, btn_rank_hover, btn_ok_warning, btn_ok_warning_hover; 
-	public static BufferedImage popup_warning;
+	public static BufferedImage popup_warning, left_panel;
 	public Image wave_animation = new ImageIcon(getClass().getResource("/textures/wave_animation.gif")).getImage();
 	public static Font military_font30;
 
@@ -23,6 +23,8 @@ public class Assets {
 		SpriteSheet targetsSheet = new SpriteSheet(ImageLoader.loadImage("/textures/targets_sprite_sheet.png"));
 		SpriteSheet userAvatarSheet  = new SpriteSheet(ImageLoader.loadImage("/textures/spritesheet_user_avatar.png"));
 		SpriteSheet btnSexSheet  = new SpriteSheet(ImageLoader.loadImage("/textures/spritesheet_btn_sex.png"));
+		
+		SpriteSheet panelsSheet  = new SpriteSheet(ImageLoader.loadImage("/textures/left_panel.png"));
 		
 		SpriteSheet btnStartSheet = new SpriteSheet(ImageLoader.loadImage("/textures/btn_start.png"));
 		SpriteSheet btnStartSheetHover = new SpriteSheet(ImageLoader.loadImage("/textures/btn_start_hover.png"));
@@ -36,7 +38,7 @@ public class Assets {
 		SpriteSheet popupsSheet = new SpriteSheet(ImageLoader.loadImage("/textures/popup_warning.png"));
 		
 		popup_warning = popupsSheet.crop(0, 0, 1129, 585);
-				
+		left_panel = panelsSheet.crop(0, 0, 300, 840);
 		
 		btn_start = btnStartSheet.crop(0, 0, 454, 166);
 		btn_start_hover = btnStartSheetHover.crop(0, 0, 454, 166);
